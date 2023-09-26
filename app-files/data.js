d40cbc9d7cf89b33e2a6ecd946af1fb245d6800e
i.deshchenya <i.deshchenya@itransition.com>
2023-09-26 21:33:26 +0300

    initial commit

---
+++ app-files/data.js
@@ -0,0 +1,78 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-koridor",
+      "name": "koridor",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 320,
+      "initialViewParameters": {
+        "yaw": 1.6004452846225572,
+        "pitch": 0.09540701291722797,
+        "fov": 1.4146236777971652
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.45350177624123766,
+          "pitch": 0.4074615739908136,
+          "rotation": 0,
+          "target": "1-timothy-oldfield-luufnhochru-unsplash"
+        },
+        {
+          "yaw": -1.6327764123210038,
+          "pitch": 0.16094822699536593,
+          "rotation": 0,
+          "target": "1-timothy-oldfield-luufnhochru-unsplash"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-timothy-oldfield-luufnhochru-unsplash",
+      "name": "timothy-oldfield-luufnHoChRU-unsplash",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
